a5a551990be0ba282e0bf08b59127863bbc3a36b
Shane Mendez <mendezsoftware@gmail.com>
2022-10-27 16:01:50 -0500

    updated index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import * as types from './types/themelio-types';
+export * from './types/themelio-types';
 export * from './melwalletd-interfaces';
 export * from './utils/utils';
 export * from './utils/wallet-utils';
 export * from './types/melwalletd-types';
 
 
-console.log(types)
